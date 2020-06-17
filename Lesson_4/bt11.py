2e4c5487d01790d61e10803974013e89b6eaa901
duyloc <loc.hoangduyloc@gmail.com>
2020-06-17 23:51:01 +0700

    Sua bai

--- Lesson_4/bt11.py
+++ Lesson_4/bt11.py
@@ -1,5 +1,7 @@
 # Bài 03: Viết chương trình tạo ra list mới bằng cách ghép một chuỗi s vào các phần tử list cũ.
 chuoi = input("Nhap chuoi :")
 my_list = [1,2,3]
-my_list.append(chuoi)
-print(my_list)
+your_list = []
+for i in my_list :
+    your_list.append(str(i)+chuoi) 
+print(your_list)
